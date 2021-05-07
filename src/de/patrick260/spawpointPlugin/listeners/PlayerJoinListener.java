b96185081dc147578dd8409b25385c61abf2cb34
patrick <Patrick260@Protonmail.com>
2021-05-07 18:35:38 +0200

    Added a join listener who teleport the players to the spawn when they join

---
+++ src/de/patrick260/spawpointPlugin/listeners/PlayerJoinListener.java
@@ -0,0 +1,24 @@
+package de.patrick260.spawpointPlugin.listeners;
+
+import de.patrick260.spawpointPlugin.main.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoinListener implements Listener {
+
+    private FileConfiguration config = Main.getPlugin().getConfig();
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+
+        Location location = new Location(Bukkit.getWorld(config.getString("data.spawnpoint.world")), config.getDouble("data.spawnpoint.x"), config.getDouble("data.spawnpoint.y"), config.getDouble("data.spawnpoint.z"), config.getFloat("data.spawnpoint.yaw"), config.getFloat("data.spawnpoint.pitch"));
+
+        event.getPlayer().teleport(location);
+
+    }
+
+}
