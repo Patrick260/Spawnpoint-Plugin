918aee0aea84ad7cbd1c90e5d7c504d75cc2701b
Patrick260 <Patrick260@protonmail.com>
2021-05-07 15:07:29 +0200

    Setup some other basics of the plugin in Main.java

--- src/de/patrick260/spawpointPlugin/main/Main.java
+++ src/de/patrick260/spawpointPlugin/main/Main.java
@@ -2,6 +2,7 @@ package de.patrick260.spawpointPlugin.main;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -10,8 +11,9 @@ public class Main extends JavaPlugin {
     private static Main plugin;
 
     private ConsoleCommandSender console = Bukkit.getConsoleSender();
+    private FileConfiguration config = getConfig();
 
-    private String plugin_prefix = "§7[§2SpawnpointPlugin§7] §f";
+    private String plugin_prefix;
 
     //A text that display when you try to start the plugin as a Java programm.
     public static void main(String[] args) {
@@ -24,20 +26,60 @@ public class Main extends JavaPlugin {
     //Things that happens when the server load the plugin.
     public void onEnable() {
 
+        setupDefaultConfig();
+        console.sendMessage("[SpawnpointPlugin] config.yml wurde erfolgreich geladen!");
 
+        plugin_prefix = getPrefix();
+        console.sendMessage(plugin_prefix + "§aDer Plugin Prefix wurde erfolgreich aus der Config geladen!");
+
+        registerCommands();
+        registerListener();
 
     }
 
     //Things that happens when the server disable the plugin.
     public void onDisable() {
 
+
+
     }
 
+
+    //Register the commands of the plugin.
+    private void registerCommands() {
+
+    }
+
+    //Register the listeners of the plugin.
+    private void registerListener() {
+
+    }
+
+    //Setup the default config file
+    private void setupDefaultConfig() {
+
+        config.addDefault("settings.prefix", "§7[§2SpawnpointPlugin§7] §f");
+
+        config.options().copyDefaults(true);
+
+        saveConfig();
+
+    }
+
+
+    //Getter and Setter
 
     //A Methode to get the main-singelton.
     public static Main getPlugin() {
 
         return plugin;
+
+    }
+
+    //Returns the plugin prefix
+    public String getPrefix() {
+
+        return config.getString("settings.prefix");
 
     }
 
