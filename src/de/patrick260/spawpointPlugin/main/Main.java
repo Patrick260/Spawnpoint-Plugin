ca9f96df85a6e7b05374d7e825d7e8c7e1fcdf80
Patrick260 <Patrick260@protonmail.com>
2021-05-07 14:42:10 +0200

    create Main.java

---
+++ src/de/patrick260/spawpointPlugin/main/Main.java
@@ -0,0 +1,10 @@
+package de.patrick260.spawpointPlugin.main;
+
+public class Main {
+
+    //A text that display when you try to start the plugin as a Java programm.
+    public static void main(String[] args) {
+        System.out.println("This is a Minecraft 1.8.8 plugin! Not a Java programm!");
+    }
+
+}
