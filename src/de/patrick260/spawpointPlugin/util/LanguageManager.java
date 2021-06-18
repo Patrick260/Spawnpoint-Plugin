c1beb3fdacf6326027176defb036aee6d79342fe
Patrick260 <Patrick260@protonmail.com>
2021-06-18 21:55:57 +0200

    Add a warn and auto disable when the server loads the plugin and a invalid language was selected in the config.yml

--- src/de/patrick260/spawpointPlugin/util/LanguageManager.java
+++ src/de/patrick260/spawpointPlugin/util/LanguageManager.java
@@ -1,5 +1,6 @@
 package de.patrick260.spawpointPlugin.util;
 
+import de.patrick260.spawpointPlugin.main.Main;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -13,7 +14,16 @@ public class LanguageManager {
 
         this.language = language;
 
-        languageFile = YamlConfiguration.loadConfiguration(this.getClass().getClassLoader().getResourceAsStream("LANG/LANG_" + language + ".yml"));
+        try {
+
+            languageFile = YamlConfiguration.loadConfiguration(this.getClass().getClassLoader().getResourceAsStream("LANG/LANG_" + language + ".yml"));
+
+        } catch (IllegalArgumentException exception) {
+
+            Main.getPlugin().getConsole().sendMessage(Main.getPlugin().getPrefix() + "§cAn error occurred while loading the language file! Perhaps an invalid language was selected.");
+            Main.getPlugin().getPluginLoader().disablePlugin(Main.getPlugin());
+
+        }
 
     }
 
