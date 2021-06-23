28e5c34897703c5396ba5a2c665861fdb3acaed0
patrick <Patrick260@Protonmail.com>
2021-06-23 20:30:37 +0200

    Create SpawnpointInfoCommand.java

---
+++ src/de/patrick260/spawpointPlugin/commands/SpawnpointInfoCommand.java
@@ -0,0 +1,17 @@
+package de.patrick260.spawpointPlugin.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class SpawnpointInfoCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+
+        return true;
+
+    }
+
+}
